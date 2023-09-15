c820811c778a5c190231b23caacd0995afa3526e
daehan.lawplatform <daehan@lawplatform.kr>
2023-09-15 13:54:54 +0900

    remove Nav in the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Provider from './_trpc/Provider';
-import Nav, { NavigationMenuDemo } from '@/src/components/Nav';
+import { NavigationMenuDemo } from '@/src/components/Nav';
 
 const inter = Inter({ subsets: ['latin'] })
 
